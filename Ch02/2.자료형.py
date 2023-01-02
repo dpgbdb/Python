086b944238a94ea15f92914cb962b995b9663061
kimdm505 <rlaehdals606@gmail.com>
2023-01-02 18:47:03 +0900

    2023 -01 -02 파이썬실습

---
+++ Ch02/2.자료형.py
@@ -0,0 +1,48 @@
+"""
+날짜 : 2023/01/02
+이름 : 김동민
+내용 : 파이썬 자료형 실습
+"""
+
+# 정수형 
+var1 = 1
+var2 = 2
+var3 = 3
+
+print('var1 : ',var1)
+print('var2 : ',var2)
+print('var3 : ',var3)
+print('var1 type : ',type(var1))
+print('var2 type : ',type(var2))
+print('var3 type : ',type(var3))
+# 실수형 
+var4 = 0.4
+var5 = 1.123456789
+
+print('var4 : ',var4)
+print('var5 : ',var5)
+print('var4 : ',type(var4))
+print('var5 : ',type(var5))
+# 논리형 
+var6 = True
+var7 = False
+
+print('var6 : ',var6)
+print('var7 : ',var7)
+print('var6 : ',type(var6))
+print('var7 : ',type(var7))
+
+# 문자형 
+str1 = 'A'
+str2 = 'Apple'
+str3 = "Apple"
+str4 = "사과"
+
+print('str1 : ', str1)
+print('str2 : ', str2)
+print('str3 : ', str3)
+print('str4 : ', str4)
+print('str1 type : ', type(str1))
+print('str2 type : ', type(str2))
+print('str3 type : ', type(str3))
+print('str4 type : ', type(str4))
